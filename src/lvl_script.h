085c401199f43b1782acdeaa511ca35c72b1dd7a
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-26 07:17:12 +0200

    Update lvl_script.h
    it's time to see what the game is really capable of

--- src/lvl_script.h
+++ src/lvl_script.h
@@ -33,9 +33,9 @@ extern "C" {
 /******************************************************************************/
 #define PARTY_TRIGGERS_COUNT     256
 #define CREATURE_PARTYS_COUNT    256
-#define CONDITIONS_COUNT         255
+#define CONDITIONS_COUNT         32767
 #define TUNNELLER_TRIGGERS_COUNT 256
-#define SCRIPT_VALUES_COUNT      1024
+#define SCRIPT_VALUES_COUNT      32767
 #define WIN_CONDITIONS_COUNT      4
 
 #define CONDITION_ALWAYS (CONDITIONS_COUNT)

e63d65f25ee0b88e88f1c723491f136eb187d86f
walter253 <130906143+walt253@users.noreply.github.com>
2023-12-21 23:35:17 +0100

    Update packets.c

--- src/packets.c
+++ src/packets.c
@@ -189,20 +189,20 @@ TbBool process_dungeon_control_packet_spell_overcharge(long plyr_idx)
       case PSt_Armour:
           update_power_overcharge(player, PwrK_PROTECT);
           break;
+      case PSt_Rebound:
+          update_power_overcharge(player, PwrK_REBOUND);
+          break;
       case PSt_Conceal:
           update_power_overcharge(player, PwrK_CONCEAL);
           break;
       case PSt_Heal:
           update_power_overcharge(player, PwrK_HEALCRTR);
           break;
-      case PSt_TimeBomb:
-          update_power_overcharge(player, PwrK_TIMEBOMB);
-          break;
-      case PSt_Rebound:
-          update_power_overcharge(player, PwrK_REBOUND);
-          break;
       case PSt_Freeze:
           update_power_overcharge(player, PwrK_FREEZE);
+          break;
+      case PSt_TimeBomb:
+          update_power_overcharge(player, PwrK_TIMEBOMB);
           break;
       default:
           player->cast_expand_level++;

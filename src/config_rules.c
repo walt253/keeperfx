6d1f1d9df054440ba8115ce6b0819f4813bf73ce
walter253 <130906143+walt253@users.noreply.github.com>
2024-01-07 07:21:09 +0100

    Update config_rules.c

--- src/config_rules.c
+++ src/config_rules.c
@@ -79,6 +79,8 @@ const struct NamedCommand rules_game_commands[] = {
   {"ALLIESSHAREDROP",            35},
   {"ALLIESSHARECTA",             36},
   {"MAXTHINGSINHAND",            37},
+  {"TORTURETRAININGCOST",        38},
+  {"TORTURESCAVENGINGCOST",      39},
   {NULL,                          0},
   };
 
@@ -757,6 +759,32 @@ TbBool parse_rules_game_blocks(char *buf, long len, const char *config_textname,
             {
               CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
                   COMMAND_TEXT(cmd_num),block_buf,config_textname);
+            }
+            break;
+        case 38: // TORTURETRAININGCOST
+            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                k = atoi(word_buf);
+                game.conf.rules.game.torture_training_cost = (TbBool)k;
+                n++;
+            }
+            if (n < 1)
+            {
+                CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
+                    COMMAND_TEXT(cmd_num), block_buf, config_textname);
+            }
+            break;
+        case 39: // TORTURESCAVENGINGCOST
+            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                k = atoi(word_buf);
+                game.conf.rules.game.torture_scavenging_cost = (TbBool)k;
+                n++;
+            }
+            if (n < 1)
+            {
+                CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
+                    COMMAND_TEXT(cmd_num), block_buf, config_textname);
             }
             break;
         case 0: // comment

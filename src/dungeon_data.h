0a892f3e28a5370403f109dd68e1c0e3ab8c9192
walter253 <130906143+walt253@users.noreply.github.com>
2024-01-13 09:51:47 +0100

    SetPlayerModifier

--- src/dungeon_data.h
+++ src/dungeon_data.h
@@ -256,12 +256,20 @@ struct Dungeon {
     unsigned char devastation_centr_y;
     unsigned long devastation_turn;
     long creatures_total_pay;
-unsigned short gold_hoard_for_pickup;
-unsigned long gold_pickup_amount;
+    unsigned short gold_hoard_for_pickup;
+    unsigned long gold_pickup_amount;
     /** Index of last creature picked up of given model. */
     unsigned short selected_creatures_of_model[CREATURE_TYPES_MAX];
     /** Index of last creature picked up of given GUI Job. */
     unsigned short selected_creatures_of_gui_job[CREATURE_GUI_JOBS_COUNT];
+    /** Used to set player modifier with script command. */
+    short modifier_melee_damage;
+    short modifier_spell_damage;
+    short modifier_speed;
+    short modifier_pay;
+    short modifier_training_cost;
+    short modifier_scavenging_cost;
+    short modifier_loyalty;
     unsigned char texture_pack;
     unsigned char color_idx;
     struct TrapInfo       mnfct_info;

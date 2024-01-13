70d6729fdc0d6365d1743afea072c649a1774df0
walter253 <130906143+walt253@users.noreply.github.com>
2024-01-13 10:10:00 +0100

    Update thing_stats.h

--- src/thing_stats.h
+++ src/thing_stats.h
@@ -88,7 +88,7 @@ long compute_creature_attack_melee_damage(long base_param,long luck,unsigned sho
 long compute_creature_attack_spell_damage(long base_param,long luck,unsigned short crlevel, struct Thing* thing);
 long project_creature_attack_melee_damage(long base_param,long luck,unsigned short crlevel, const struct Thing* thing);
 long project_creature_attack_spell_damage(long base_param,long luck,unsigned short crlevel, const struct Thing* thing);
-long compute_creature_attack_range(long base_param,long luck,unsigned short crlevel);
+long compute_creature_attack_range(long base_param,long luck,unsigned short crlevel, struct Thing* thing);
 long compute_creature_work_value(long base_param,long efficiency,unsigned short crlevel);
 long compute_creature_max_dexterity(long base_param,unsigned short crlevel);
 long compute_creature_max_defense(long base_param,unsigned short crlevel);

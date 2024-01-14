adc67f1b505aff57eaba85d4d1fcd7f8c702352d
walter253 <130906143+walt253@users.noreply.github.com>
2024-01-14 16:28:48 +0100

    Update thing_shots.h

--- src/thing_shots.h
+++ src/thing_shots.h
@@ -58,10 +58,11 @@ enum ShotModels {
     ShM_Disease,
     ShM_Chicken,
     ShM_TimeBomb,
-    ShM_TrapLightning, // 29
-    ShM_WordOfPower, 
+    ShM_TrapLightning,
+    ShM_WordOfPower, // 30
     ShM_TrapWordOfPower,
     ShM_TrapTNT = 32,
+    ShM_GodMeteor = 33,
 };
 
 enum ShotFireLogics {

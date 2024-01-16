084178cad29f61ae8df4d9ae50f9fd0105714c26
walter253 <130906143+walt253@users.noreply.github.com>
2024-01-16 13:52:27 +0100

    Update packets.c

--- src/packets.c
+++ src/packets.c
@@ -213,8 +213,8 @@ TbBool process_dungeon_control_packet_spell_overcharge(long plyr_idx)
       case PSt_Vision:
           update_power_overcharge(player, PwrK_VISION);
           break;
-      case PSt_Meteor:
-          update_power_overcharge(player, PwrK_METEOR);
+      case PSt_MeteorStorm:
+          update_power_overcharge(player, PwrK_METEORSTORM);
           break;
       default:
           player->cast_expand_level++;

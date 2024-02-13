51b997afd0cbe2b5d066a812a61bd6acc183d24e
walter253 <130906143+walt253@users.noreply.github.com>
2024-02-13 19:09:33 +0100

    Update thing_effects.c

--- src/thing_effects.c
+++ src/thing_effects.c
@@ -369,10 +369,31 @@ void process_spells_affected_by_effect_elements(struct Thing *thing)
 
     if ((cctrl->spell_flags & CSAfF_MagicMist) != 0)
     {
-        pos.x.val = thing->mappos.x.val;
-        pos.y.val = thing->mappos.y.val;
+        int diamtr = thing->clipbox_size_xy / 2;
+        dturn = game.play_gameturn - thing->creation_turn;
+        MapCoord cor_z_max = thing->clipbox_size_z + (thing->clipbox_size_z * game.conf.crtr_conf.exp.size_increase_on_exp * cctrl->explevel) / 120; //effect is 25% larger than unit
+
+        struct EffectElementConfigStats* eestat = get_effect_element_model_stats(TngEffElm_CloudDisperse);
+        unsigned short nframes = keepersprite_frames(eestat->sprite_idx);
+        GameTurnDelta dtadd = 0;
+        unsigned short cframe = game.play_gameturn % nframes;
         pos.z.val = thing->mappos.z.val;
-        effeltng = create_thing(&pos, TCls_EffectElem, TngEffElm_EntranceMist, thing->owner, -1);
+        int radius = diamtr / 3;
+        while (pos.z.val < cor_z_max + thing->mappos.z.val)
+        {
+            angle = (abs(dturn + dtadd) & 7) << 8;
+            shift_x =  (radius * LbSinL(angle) >> 8) >> 8;
+            shift_y = -(radius * LbCosL(angle) >> 8) >> 8;
+            pos.x.val = thing->mappos.x.val + shift_x;
+            pos.y.val = thing->mappos.y.val + shift_y;
+            effeltng = create_thing(&pos, TCls_EffectElem, TngEffElm_CloudDisperse, thing->owner, -1);
+            if (thing_is_invalid(effeltng))
+                break;
+            set_thing_draw(effeltng, eestat->sprite_idx, 256, eestat->sprite_size_min, 0, cframe, ODC_Default);
+            dtadd++;
+            pos.z.val += 32;
+            cframe = (cframe + 1) % nframes;
+        }
     }
 }
 

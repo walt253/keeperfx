167407e7e08fdd5b1623cf1f1e5765a0fff90ffc
walter253 <130906143+walt253@users.noreply.github.com>
2024-02-23 06:13:54 +0100

    Update magic.c

--- src/magic.c
+++ src/magic.c
@@ -1423,6 +1423,11 @@ TbResult magic_use_power_freeze(PlayerNumber plyr_idx, struct Thing *thing, MapS
             return Lb_FAIL;
         }
     }
+    struct CreatureStats* crstat = creature_stats_get_from_thing(thing);
+    if (crstat->immune_to_freeze != 0) {
+        thing_play_sample(thing, 119, NORMAL_PITCH, 0, 3, 0, 2, FULL_LOUDNESS);
+        return Lb_FAIL;
+    }
     struct Coord3d effpos = thing->mappos;
     effpos.z.val = get_ceiling_height_above_thing_at(thing, &thing->mappos);
     create_effect(&effpos, TngEff_FallingIceBlocks, thing->owner);
@@ -1445,6 +1450,11 @@ TbResult magic_use_power_slow(PlayerNumber plyr_idx, struct Thing *thing, MapSub
         if (!pay_for_spell(plyr_idx, PwrK_SLOW, splevel)) {
             return Lb_FAIL;
         }
+    }
+    struct CreatureStats* crstat = creature_stats_get_from_thing(thing);
+    if (crstat->immune_to_slow != 0) {
+        thing_play_sample(thing, 119, NORMAL_PITCH, 0, 3, 0, 2, FULL_LOUDNESS);
+        return Lb_FAIL;
     }
     struct Coord3d effpos = thing->mappos;
     effpos.z.val = get_ceiling_height_above_thing_at(thing, &thing->mappos);

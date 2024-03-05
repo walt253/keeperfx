a87f2ad234ad908f595f0dc4a050ac53609ee0be
walter253 <130906143+walt253@users.noreply.github.com>
2024-03-05 16:08:48 +0100

    Update thing_traps.c

--- src/thing_traps.c
+++ src/thing_traps.c
@@ -776,7 +776,10 @@ TngUpdateRet update_trap_trigger(struct Thing* traptng)
     case TrpTrg_LineOfSight:
         do_trig = update_trap_trigger_line_of_sight(traptng);
         break;
-    case TrpTrg_None: // for manually activated traps
+    case TrpTrg_WheneverPlaced: // Activated once placed.
+        do_trig = true;
+        break;
+    case TrpTrg_None: // For manually activated traps.
         do_trig = false;
         break;
     default:

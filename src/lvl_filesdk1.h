3a04709529ea6ecdae9aac3cd2a2ffd1fae1c433
walter253 <130906143+walt253@users.noreply.github.com>
2024-10-06 03:38:58 +0200

    Update lvl_filesdk1.h

--- src/lvl_filesdk1.h
+++ src/lvl_filesdk1.h
@@ -28,8 +28,8 @@
 extern "C" {
 #endif
 /******************************************************************************/
-#define MAX_LIF_SIZE          65535
-#define ANY_MAP_FILE_MAX_SIZE 2147483647
+#define MAX_LIF_SIZE 65535
+#define ANY_MAP_FILE_MAX_SIZE 1048576
 #define DEFAULT_LEVEL_VERSION 0
 
 enum LoadMapFileFlags {

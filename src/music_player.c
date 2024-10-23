cf32033417d8b65e7c2c41b3df5da448f028eaec
walter253 <130906143+walt253@users.noreply.github.com>
2024-10-24 00:29:16 +0200

    Update music_player.c

--- src/music_player.c
+++ src/music_player.c
@@ -166,7 +166,6 @@ void SetMusicPlayerVolume(int volume)
     {
         float volume_f = (float) volume;
         int normalized_volume = (int)((volume_f / MIX_MAX_VOLUME) * MIX_MAX_VOLUME);
-        int old_volume = Mix_VolumeMusic(-1);
         Mix_VolumeMusic(normalized_volume);
     }
 }

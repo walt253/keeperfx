e17fb7af177ed95b95352c9931ddd02f70170c9c
walter253 <130906143+walt253@users.noreply.github.com>
2024-11-11 09:53:17 +0100

    crmodel

--- src/creature_states.c
+++ src/creature_states.c
@@ -4131,9 +4131,9 @@ TbBool process_creature_hunger(struct Thing *thing)
 TbBool creature_is_hostile_towards(const struct Thing *fightng, const struct Thing *enmtng)
 {
     struct CreatureStats* crstat = creature_stats_get_from_thing(fightng);
-    for (ThindModel crmodel = 1; i < game.conf.crtr_conf.model_count; crmodel++)
+    for (ThindModel crmodel = 1; crmodel < game.conf.crtr_conf.model_count; crmodel++)
     {
-        if ((crstat->hostile_towards[i] == enmtng->model) || (crstat->hostile_towards[i] == CREATURE_ANY))
+        if ((crstat->hostile_towards[crmodel] == enmtng->model) || (crstat->hostile_towards[crmodel] == CREATURE_ANY))
         {
             return true;
         }

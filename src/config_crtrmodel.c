5d7050fc75735a9a1a36fd7f1cb3cc4d17ca45a1
walter253 <130906143+walt253@users.noreply.github.com>
2024-11-19 15:29:41 +0100

    Update config_crtrmodel.c

--- src/config_crtrmodel.c
+++ src/config_crtrmodel.c
@@ -1778,7 +1778,7 @@ TbBool parse_creaturemodel_appearance_blocks(long crtr_model,char *buf,long len,
             if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
             {
                 k = atoi(word_buf);
-                if (k > 0)
+                if (k >= 0)
                 {
                     crstat->fixed_anim_speed = k;
                 }

8fbcd6a3a81da16849ffe1eea274fac5f0ad063c
walter253 <130906143+walt253@users.noreply.github.com>
2024-11-22 21:53:30 +0100

    Update creature_states_lair.c

--- src/creature_states_lair.c
+++ src/creature_states_lair.c
@@ -519,7 +519,10 @@ short creature_sleep(struct Thing *thing)
         return 0;
     }
     struct Room *room = get_room_thing_is_on(thing);
-    if (room_is_invalid(room) || (!room_role_matches(room->kind, get_room_role_for_job(Job_TAKE_SLEEP))) || (cctrl->lair_room_id != room->index) || (room->owner != thing->owner))
+    if (room_is_invalid(room)
+    || (!room_role_matches(room->kind, get_room_role_for_job(Job_TAKE_SLEEP)))
+    || (cctrl->lair_room_id != room->index)
+    || (room->owner != thing->owner))
     {
         set_start_state(thing);
         return 0;

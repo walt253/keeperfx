a8031ac89430c04e8744be1028bcac83c456b9a2
walter253 <130906143+walt253@users.noreply.github.com>
2024-12-09 23:33:11 +0100

    Update config_magic.c

--- src/config_magic.c
+++ src/config_magic.c
@@ -65,6 +65,30 @@ const struct NamedCommand magic_spell_commands[] = {
   {NULL,              0},
   };
 
+const struct NamedCommand magic_spell_flags[] = {
+    {"SLOW",          1},
+    {"SPEED",         2},
+    {"ARMOUR",        3},
+    {"REBOUND",       4},
+    {"FLYING",        5},
+    {"INVISIBILITY",  6},
+    {"SIGHT",         7},
+    {"LIGHT",         8},
+    {"DISEASE",       9},
+    {"CHICKEN",      10},
+    {"POISON_CLOUD", 11},
+    {"CALL_TO_ARMS", 12},
+    {"MAD_KILLING",  13},
+    {"HEAL",         14},
+    {"EXP_LEVEL_UP", 15},
+    {"TELEPORT",     16},
+    {"TIMEBOMB",     17},
+    {"WIND",         18},
+    {"FREEZE",       19},
+    {"FEAR",         20},
+    {NULL,            0},
+};
+
 const struct NamedCommand magic_shot_commands[] = {
   {"NAME",                   1},
   {"HEALTH",                 2},
@@ -647,22 +671,112 @@ TbBool parse_magic_spell_blocks(char *buf, long len, const char *config_textname
                   COMMAND_TEXT(cmd_num), blocknamelen, blockname, config_textname);
           }
           break;
-      case 10: // SPELLFLAGS
-          if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
-          {
-              k = atoi(word_buf);
-              if (k >= 0)
-              {
-                  spconf->spell_flags = k;
-                  n++;
-              }
-          }
-          if (n < 1)
-          {
-              CONFWRNLOG("Couldn't read \"%s\" parameter in [%.*s] block of %s file.",
-                  COMMAND_TEXT(cmd_num), blocknamelen, blockname, config_textname);
-          }
-          break;
+        case 10: // SPELLFLAGS
+            spconf->spell_flags = 0;
+            while (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                if (parameter_is_number(word_buf))
+                {
+                    k = atoi(word_buf);
+                    spconf->spell_flags = k;
+                    n++;
+                }
+                else
+                {
+                    k = get_id(magic_spell_flags, word_buf);
+                    switch (k)
+                    {
+                    case 1: // SLOW
+                        set_flag(spconf->spell_flags, CSAfF_Slow);
+                        n++;
+                        break;
+                    case 2: // SPEED
+                        set_flag(spconf->spell_flags, CSAfF_Speed);
+                        n++;
+                        break;
+                    case 3: // ARMOUR
+                        set_flag(spconf->spell_flags, CSAfF_Armour);
+                        n++;
+                        break;
+                    case 4: // REBOUND
+                        set_flag(spconf->spell_flags, CSAfF_Rebound);
+                        n++;
+                        break;
+                    case 5: // FLYING
+                        set_flag(spconf->spell_flags, CSAfF_Flying);
+                        n++;
+                        break;
+                    case 6: // INVISIBILITY
+                        set_flag(spconf->spell_flags, CSAfF_Invisibility);
+                        n++;
+                        break;
+                    case 7: // SIGHT
+                        set_flag(spconf->spell_flags, CSAfF_Sight);
+                        n++;
+                        break;
+                    case 8: // LIGHT
+                        set_flag(spconf->spell_flags, CSAfF_Light);
+                        n++;
+                        break;
+                    case 9: // DISEASE
+                        set_flag(spconf->spell_flags, CSAfF_Disease);
+                        n++;
+                        break;
+                    case 10: // CHICKEN
+                        set_flag(spconf->spell_flags, CSAfF_Chicken);
+                        n++;
+                        break;
+                    case 11: // POISON_CLOUD
+                        set_flag(spconf->spell_flags, CSAfF_PoisonCloud);
+                        n++;
+                        break;
+                    case 12: // CALL_TO_ARMS
+                        set_flag(spconf->spell_flags, CSAfF_CalledToArms);
+                        n++;
+                        break;
+                    case 13: // MAD_KILLING
+                        set_flag(spconf->spell_flags, CSAfF_MadKilling);
+                        n++;
+                        break;
+                    case 14: // HEAL
+                        set_flag(spconf->spell_flags, CSAfF_Heal);
+                        n++;
+                        break;
+                    case 15: // EXP_LEVEL_UP
+                        set_flag(spconf->spell_flags, CSAfF_ExpLevelUp);
+                        n++;
+                        break;
+                    case 16: // TELEPORT
+                        set_flag(spconf->spell_flags, CSAfF_Teleport);
+                        n++;
+                        break;
+                    case 17: // TIMEBOMB
+                        set_flag(spconf->spell_flags, CSAfF_Timebomb);
+                        n++;
+                        break;
+                    case 18: // WIND
+                        set_flag(spconf->spell_flags, CSAfF_Wind);
+                        n++;
+                        break;
+                    case 19: // FREEZE
+                        set_flag(spconf->spell_flags, CSAfF_Freeze);
+                        n++;
+                        break;
+                    case 20: // FEAR
+                        set_flag(spconf->spell_flags, CSAfF_Fear);
+                        n++;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            if (n < 1)
+            {
+                CONFWRNLOG("Couldn't read \"%s\" parameter in [%.*s] block of %s file.",
+                    COMMAND_TEXT(cmd_num), blocknamelen, blockname, config_textname);
+            }
+            break;
         case 11: // SUMMONCREATURE
           if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
           {

ba050d237c89587e2f442d647ee8786b7c4a0912
walter253 <130906143+walt253@users.noreply.github.com>
2025-01-08 19:40:05 +0100

    Update config_magic.c

--- src/config_magic.c
+++ src/config_magic.c
@@ -69,6 +69,7 @@ const struct NamedCommand magic_spell_commands[] = {
     {"AURAFREQUENCY",   17},
     {"CLEANSEFLAGS",    18},
     {"PROPERTIES",      19},
+    {"TRANSFORM",       20},
     {NULL,               0},
 };
 
@@ -906,6 +907,37 @@ TbBool parse_magic_spell_blocks(char *buf, long len, const char *config_textname
             if (n < 1)
             {
                 CONFWRNLOG("Couldn't read \"%s\" parameter in [%.*s] block of %s file.",
+                    COMMAND_TEXT(cmd_num), blocknamelen, blockname, config_textname);
+            }
+            break;
+        case 20: // TRANSFORM
+            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                if (parameter_is_number(word_buf))
+                {
+                    k = atoi(word_buf);
+                    spconf->transform_model = k;
+                    n++;
+                }
+                else
+                {
+                    k = get_id(creature_desc, word_buf);
+                    if (k >= 0)
+                    {
+                        spconf->transform_model = k;
+                        n++;
+                    }
+                }
+            }
+            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                k = atoi(word_buf);
+                spconf->transform_level = k;
+                n++;
+            }
+            if (n < 2)
+            {
+                CONFWRNLOG("Incorrect value of \"%s\" parameter in [%.*s] block of %s file.",
                     COMMAND_TEXT(cmd_num), blocknamelen, blockname, config_textname);
             }
             break;

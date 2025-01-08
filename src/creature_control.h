44328aeedeadd6aa0c82e100199eeea53078b150
walter253 <130906143+walt253@users.noreply.github.com>
2025-01-08 19:45:26 +0100

    Update creature_control.h

--- src/creature_control.h
+++ src/creature_control.h
@@ -135,6 +135,7 @@ struct CastedSpellData {
     GameTurnDelta duration;
     CrtrExpLevel caster_level;
     PlayerNumber caster_owner;
+    ThingModel caster_model;
 };
 
 struct CreatureControl {
